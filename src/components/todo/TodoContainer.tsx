27801e0475b23c23a4e2a99b0112b5ae19eac06c
Kakon Debnath <kakondebnath96@gmail.com>
2024-02-07 17:39:49 +0600

    add todo filter

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,13 +1,14 @@
 import { Button } from '../ui/button';
 import AddTodoModal from './AddTodoModal';
 import TodoCard from './TodoCard';
+import TodoFilter from './TodoFilter';
 
 const TodoContainer = () => {
   return (
     <div>
       <div className="flex justify-between items-center py-3">
         <AddTodoModal />
-        <Button className="bg-primary-gradient"> Filter</Button>
+        <TodoFilter />
       </div>
       <div className=" bg-primary-gradient w-full rounded-xl p-1">
         <div className="flex flex-col gap-1 bg-white rounded-lg p-2">
